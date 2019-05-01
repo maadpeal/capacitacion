81c23a552ef510d74eaa49a47f8353d5febc4efb
Adrian Matute <adrian23269@gmail.com>
2019-05-01 15:28:11 -0300

    Commit de clase catalogoNetflix y DroneTest

--- ModeloExamen2/CatalogoNetflix.php
+++ ModeloExamen2/CatalogoNetflix.php
@@ -1,11 +1,6 @@
 <?php
 
 /**
- * Nos han pedido reemplazar la herramienta para mantener
- * el catalogo de peliculas de Netflix porque el original
- * es muy malo. Pero como es un cambio muy grande en nuestra
- * primera entrega no hay que entregar todas las funcionalidades.
- * 
  * Las funcionalidades que nos piden son:
  *  - Agregar peliculas nuevas
  *  - Agregar series nuevas
@@ -15,12 +10,11 @@
  *  - Una funcion que te dice si existe el id de pelicula/serie
  * 
  * Las categorias se van a ir creando a medida que se agregan
- * peliculas o series, entonces si se agrega una serie con la
+ * peliculas o series,
+ * 
+ *  entonces si se agrega una serie con la
  * categoria "ciencia misteriosa" esta categoría empieza a
  * existir en ese momento.
- * 
- * Tendremos que pasar todos los tests y tratemos de quedar
- * bien porque es nuestro primer cliente importante!
  */
 
 class CatalogoNetflix {
@@ -28,22 +22,90 @@ class CatalogoNetflix {
    * Esta funcion solo nos dice si existe la pelicula o serie con
    * el id que nos pasan
    */
-  public function existeId($id) {
+
+   private $peliculas;
+   private $series;
+   private $categorias = [];
+   
+  public function existeId($id) 
+  {
+    if((isset($this->peliculas[$id] ) || (isset($this->series[$id]))))
+    {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  public function agregarSerie($id, $nombre, $cantidadCapitulos, $categoria) {
+  public function agregarSerie($id, $nombre, $cantidadCapitulos, $categoria) 
+  {
+    if(!isset($this->series[$id]))
+    {
+      $this->series[$id] = array($nombre, $cantidadCapitulos, $categoria);
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  public function agrearPelicula($id, $nombre, $tiempo, $categoria) {
+  public function agrearPelicula($id, $nombre, $tiempo, $categoria) 
+  {
+    if(!isset($this->peliculas[$id]))
+    {
+      $this->peliculas[$id] = array($nombre, $tiempo, $categoria);
+      return true;
+    }
+    else {
+      return false;
+    }
+    
   }
 
-  public function sacarSerie($id) {
+  public function sacarSerie($id) 
+  {
+    if(isset($this->series[$id]))
+    {
+      unset($this->series[$id]);
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  public function sacarPelicula($id) {
+  public function sacarPelicula($id) 
+  {
+    if(isset($this->peliculas[$id]))
+    {
+      unset($this->peliculas[$id]);
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  public function listarContenidoDeLaCategoria($categoria) {
+  public function listarContenidoDeLaCategoria($categoria) 
+  {
+    $this->categorias = [];
+    foreach($this->peliculas as $key => $value)
+    {
+      if($value[2] === $categoria)
+      {
+        $this->categorias[] = $value[2];
+      }
+    }
+
+    foreach($this->series as $key => $value)
+    {
+      if($value[2] === $categoria)
+      {
+        $this->categorias[] = $value[2];
+      }
+    }
+    return $this->categorias;
   }
 
 }
